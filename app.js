ad97ec8e5835033c5f453e9e463bdd116b6bc315
durveshramane26 <durveshramane6@gmail.com>
2022-05-20 21:21:08 +0530

    added login and register page

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3003;
 const middleware = require('./middleware')
+const path=require('path')
 
 const server = app.listen(port, () => console.log("Server Listening on port "+ port));
 
 app.set("view engine", "pug");
 app.set("views", "views");
 
+app.use(express.static(path.join(__dirname, "public")));
+
 //Routes 
 
 const loginRoute = require('./routes/loginRoutes');
+const registerRoute = require('./routes/registerRoutes')
+
 app.use("/login", loginRoute);
+app.use("/register",registerRoute);
 
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
