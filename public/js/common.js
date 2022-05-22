0dd2e1c7080fea3b41fa4890e6f33df246b47a49
durveshramane26 <durveshramane6@gmail.com>
2022-05-23 02:36:36 +0530

    post styling done

--- public/js/common.js
+++ public/js/common.js
@@ -15,15 +15,61 @@ $("#postTextarea").keyup(event => {
 })
 
 $("#submitPostButton").click((event) => {
-    var textbox = $(event.target);
+    var button = $(event.target);
     var textbox = $("#postTextarea");
 
     var data = {
-        //content: textbox.val()
+        content: textbox.val()
     }
 
-    $.post("/api/posts", data, (postData, status, xhr) => {
-        alert(postData);
+    $.post("/api/posts", data, postData => {
+
+        var html = createPostHtml(postData);
+        $(".postsContainer").prepend(html);
+        textbox.val("");
+        button.prop("disabled", true);
 
     })
 })
+
+function createPostHtml(postData) {
+    var postedBy = postData.postedBy;
+    var displayName = postedBy.firstName + " " + postedBy.lastName;
+    var timestamp = postData.createdAt;
+
+    return `<div class='post'>
+
+                <div class='mainContentContainer'>
+                    <div class='userImageContainer'>
+                        <img src='${postedBy.profilePic}'>
+                    </div>
+                    <div class='postContentContainer'>
+                        <div class='header'>
+                            <a href='/profile/${postedBy.username}' class='displayName'>${displayName}</a>
+                            <span class='username'>@${postedBy.username}</span>
+                            <span class='date'>${timestamp}</span>
+                        </div>
+                        <div class='postBody'>
+                            <span>${postData.content}</span>
+                        </div>
+                        <div class='postFooter'>
+                            <div class='postButtonContainer'>
+                                <button>
+                                    <i class='fas fa-comment'></i>
+                                </button>
+                            </div>
+                            <div class='postButtonContainer'>
+                                <button>
+                                    <i class='fas fa-retweet'></i>
+                                </button>
+                            </div>
+                            <div class='postButtonContainer'>
+                                <button>
+                                    <i class='fas fa-heart'></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+}
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,6 +3,8 @@ const app = express();
 const router = express.Router();
 const bodyParser = require("body-parser")
 const User = require('../../schemas/UserSchema');
+const Post = require('../../schemas/PostSchema');
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -17,7 +19,23 @@ router.post("/", async (req, res, next) => {
         console.log("Content param not sent with request");
          return res.sendStatus(400);
     }
-    res.status(200).send("it worked");
+
+    var postData = {
+        content: req.body.content,
+        postedBy: req.session.user
+    }
+
+    Post.create(postData)
+    .then(async newPost => {
+        newPost = await User.populate(newPost, { path: "postedBy" })
+
+        res.status(201).send(newPost);
+    })
+    .catch(error => {
+        console.log(error);
+        res.sendStatus(400);
+    })
+    
 })
 
 module.exports = router;
