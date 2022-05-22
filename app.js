f55d2e6eb34afc0760a545a8a1004e6ecc57effa
durveshramane26 <durveshramane6@gmail.com>
2022-05-22 18:57:18 +0530

    main-layout

--- app.js
+++ app.js
@@ -26,10 +26,12 @@ app.use(session({
 //Routes 
 
 const loginRoute = require('./routes/loginRoutes');
-const registerRoute = require('./routes/registerRoutes')
+const registerRoute = require('./routes/registerRoutes');
+const logoutRoute = require('./routes/logout');
 
 app.use("/login", loginRoute);
 app.use("/register",registerRoute);
+app.use("/logout",logoutRoute);
 
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
