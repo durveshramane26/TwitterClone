f73e3e1ae51bee0f2d27892933733d6019dff8ce
durveshramane26 <durveshramane6@gmail.com>
2022-05-22 13:06:57 +0530

    logged in

--- app.js
+++ app.js
@@ -35,7 +35,8 @@ app.use("/register",registerRoute);
 app.get("/", middleware.requireLogin, (req, res, next) => {
 
     var payload = {
-        pageTitle: "Home"
+        pageTitle: "Home",
+        userLoggedIn: req.session.user 
     }
     res.status(200).render("home", payload);
 })
