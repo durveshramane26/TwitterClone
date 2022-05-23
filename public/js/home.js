d28c64fd2fa894c23f8f39959c9f1d6004e5f031
durveshramane26 <durveshramane6@gmail.com>
2022-05-23 20:46:14 +0530

    getting post from the database

---
+++ public/js/home.js
@@ -0,0 +1,5 @@
+$(document).ready(() => {
+    $.get("/api/posts", results => {
+        console.log(results);
+    })
+})
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -9,6 +9,12 @@ const Post = require('../../schemas/PostSchema');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 router.get("/", (req, res, next) => {
+    Post.find()
+    .then(results => res.status(200).send(results))
+    .catch(error => {
+        console.log(error);
+        res.sendStatus(400); 
+    })
 
     
 })
