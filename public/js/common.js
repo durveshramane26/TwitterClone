f8bb79ba1ad5a86d8eac7ec71b1c79d52c13582b
durveshramane26 <durveshramane6@gmail.com>
2022-05-25 01:02:53 +0530

    Added retweet message indicator

--- public/js/common.js
+++ public/js/common.js
@@ -115,8 +115,18 @@ function createPostHtml(postData) {
     var likeButtonActiveClass = postData.likes.includes(userLoggedIn._id) ? "active" : "";
     var retweetButtonActiveClass = postData.retweetUsers.includes(userLoggedIn._id) ? "active" : "";
 
-    return `<div class='post' data-id='${postData._id}'>
 
+    var  retweetText = '';
+    if(isRetweet) {
+        retweetText = `<span>
+                        <i class='fas fa-retweet'></i>
+                        Retweeted  by <a href='/profile/${retweetedBy}'>@${retweetedBy}</a>
+                       </span>`
+    }
+    return `<div class='post' data-id='${postData._id}'>
+                <div class='postActionContainer'>
+                    ${retweetText}
+                </div>
                 <div class='mainContentContainer'>
                     <div class='userImageContainer'>
                         <img src='${postedBy.profilePic}'>
