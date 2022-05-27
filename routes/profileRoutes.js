e8584bd9c599aa90415ed052df7eb3d96c85aeff
durveshramane26 <durveshramane6@gmail.com>
2022-05-27 14:30:22 +0530

    Handled user not found error

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -17,4 +17,31 @@ router.get("/", (req, res, next) => {
     res.status(200).render("profilePage", payload);
 })
 
+router.get("/:username", async (req, res, next) => {
+
+    var payload = await getPayload(req.params.username, req.session.user);
+    console.log(payload)
+    
+    res.status(200).render("profilePage", payload);
+})
+
+async function getPayload(username, userLoggedIn) {
+    var user = await User.findOne({ username: username })
+
+    if(user == null) {
+        return {
+            pageTitle: "User not found",
+            userLoggedIn: userLoggedIn,
+            userLoggedInJs: JSON.stringify(userLoggedIn)
+        }
+    }
+
+    return {
+        pageTitle: user.username,
+        userLoggedIn: userLoggedIn,
+        userLoggedInJs: JSON.stringify(userLoggedIn),
+        profileUser: user
+    }
+}
+
 module.exports = router;
