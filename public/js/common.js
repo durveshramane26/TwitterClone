4707e5c2b74333a16a72d5ee11b50df1d26d8bd2
durveshramane26 <durveshramane6@gmail.com>
2022-06-01 00:08:06 +0530

    Added cropper functionality

--- public/js/common.js
+++ public/js/common.js
@@ -1,3 +1,6 @@
+//Globals
+var cropper;
+
 $("#postTextarea, #replyTextarea").keyup(event => {
     var textbox = $(event.target);
     var value = textbox.val().trim();
@@ -80,6 +83,30 @@ $("#deletePostButton").click((event) => {
             location.reload();
         }
     })
+})
+
+$("#filePhoto").change(function(){    
+    if(this.files && this.files[0]) {
+        var reader = new FileReader();
+        reader.onload = (e) => {
+            var image = document.getElementById("imagePreview");
+            image.src = e.target.result;
+
+            if(cropper !== undefined) {
+                cropper.destroy();
+            }
+
+            cropper = new Cropper(image, {
+                aspectRatio: 1 / 1,
+                background: false
+            });
+
+        }
+        reader.readAsDataURL(this.files[0]);
+    }
+    else {
+        console.log("nope")
+    }
 })
 
 $(document).on("click", ".likeButton", (event) => {
