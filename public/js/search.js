df4b867db9c2cffb6af37a823417dfa6af375ccb
durveshramane26 <durveshramane6@gmail.com>
2022-06-03 01:33:43 +0530

    Added search post

--- public/js/search.js
+++ public/js/search.js
@@ -13,8 +13,24 @@ $("#searchBox").keydown((event) => {
             $(".resultsContainer").html("");
         }
         else {
-            console.log(value)
+            search(value, searchType);
         }
     }, 1000)
 
 })
+
+function search(searchTerm, searchType) {
+    var url = searchType == "users" ? "/api/users" : "/api/posts"
+
+    $.get(url, { search: searchTerm }, (results) => {
+
+
+
+        if(searchType == "users") {
+
+        }
+        else {
+            outputPosts(results, $(".resultsContainer"))
+        }
+    })
+}
