453ffd2fab0403eafd0c70dffc3d31a915ce9e64
durveshramane26 <durveshramane6@gmail.com>
2022-06-03 01:15:35 +0530

    Added search Timer

---
+++ public/js/search.js
@@ -0,0 +1,20 @@
+var timer;
+
+$("#searchBox").keydown((event) => {
+    clearTimeout(timer);
+    var textbox = $(event.target);
+    var value = textbox.val();
+    var searchType = textbox.data().search;
+
+    timer = setTimeout(() => {
+        value = textbox.val().trim();
+
+        if(value == "") {
+            $(".resultsContainer").html("");
+        }
+        else {
+            console.log(value)
+        }
+    }, 1000)
+
+})
