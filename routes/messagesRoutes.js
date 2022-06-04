b901ce25ce5c5e58a144ea37e8a8c15b55487eaf
durveshramane26 <durveshramane6@gmail.com>
2022-06-04 23:51:43 +0530

    Chat validations

--- routes/messagesRoutes.js
+++ routes/messagesRoutes.js
@@ -3,7 +3,9 @@ const app = express();
 const router = express.Router();
 const bodyParser = require("body-parser")
 const bcrypt = require("bcrypt");
+const mongoose = require('mongoose');
 const User = require('../schemas/UserSchema');
+const Chat = require('../schemas/ChatSchema');
 
 router.get("/", (req, res, next) => {
     res.status(200).render("inboxPage", {
@@ -19,6 +21,46 @@ router.get("/new", (req, res, next) => {
         userLoggedIn: req.session.user,
         userLoggedInJs: JSON.stringify(req.session.user)
     });
+})
+
+router.get("/:chatId", async (req, res, next) => {
+
+    var userId = req.session.user._id;
+    var chatId = req.params.chatId;
+    var isValidId = mongoose.isValidObjectId(chatId);
+
+
+    var payload = {
+        pageTitle: "Chat",
+        userLoggedIn: req.session.user,
+        userLoggedInJs: JSON.stringify(req.session.user)
+    };
+
+    if(!isValidId) {
+        payload.errorMessage = "Chat does not exist or you do not have permission to view it.";
+        return res.status(200).render("chatPage", payload);
+    }
+
+    var chat = await Chat.findOne({ _id: chatId, users: { $elemMatch: { $eq: userId } } })
+    .populate("users");
+
+    if(chat == null) {
+        // Check if chat id is really user id
+        var userFound = await User.findById(chatId);
+
+        if(userFound != null) {
+            // get chat using user id
+        }
+    }
+
+    if(chat == null) {
+        payload.errorMessage = "Chat does not exist or you do not have permission to view it.";
+    }
+    else {
+        payload.chat = chat;
+    }
+
+    res.status(200).render("chatPage", payload);
 })
 
 module.exports = router;
