37e5ab16165720ffa87b61e6bda07392de3721da
durveshramane26 <durveshramane6@gmail.com>
2022-06-08 12:25:10 +0530

    added port to socket

--- public/js/clientSocket.js
+++ public/js/clientSocket.js
@@ -1,6 +1,6 @@
 var connected = false;
 
-var socket = io("http://localhost:3003")
+var socket = io("https://morning-ridge-27369.herokuapp.com:8168")
 socket.emit("setup", userLoggedIn);
 
 socket.on("connected", () => connected = true);
