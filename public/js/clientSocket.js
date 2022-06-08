9d4832d2b3a0f73595c4f7a9eeb6d8c12692abc5
durveshramane26 <durveshramane6@gmail.com>
2022-06-08 13:00:45 +0530

    minor error

--- public/js/clientSocket.js
+++ public/js/clientSocket.js
@@ -1,6 +1,6 @@
 var connected = false;
-ws://your-app.herokuapp.com:9352/socket.io/?EIO=4&transport=websocket
-var socket = io("ws://https://morning-ridge-27369.herokuapp.com/socket.io/?EIO=4&transport=websocket")
+//ws://your-app.herokuapp.com:9352/socket.io/?EIO=4&transport=websocket
+var socket = io("ws://morning-ridge-27369.herokuapp.com/socket.io/?EIO=4&transport=websocket")
 socket.emit("setup", userLoggedIn);
 
 socket.on("connected", () => connected = true);
