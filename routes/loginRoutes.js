2a259f51ab819294422cc46f80b6e9a99f951277
durveshramane26 <durveshramane6@gmail.com>
2022-06-28 23:22:43 +0530

    fix: login page

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -19,6 +19,7 @@ router.get("/",(req, res, next) => {
 })
 
 router.post("/", async (req, res, next) => {
+    
     var payload = req.body;
 
     if(req.body.logUsername && req.body.logPassword) {
@@ -47,8 +48,69 @@ router.post("/", async (req, res, next) => {
         return res.status(200).render("login", payload);
     }
 
+    var firstName = req.body.firstName.trim();
+    var lastName = req.body.lastName.trim();
+    var username = req.body.username.trim();
+    var email = req.body.email.trim();
+    var password = req.body.password;
+
+    if(firstName && lastName && email && username && password) {
+        var user = await User.findOne({
+            $or: [
+                { username: username },
+                { email: email }
+            ]
+        })
+        .catch((error) => {
+            console.log(error);
+            payload.errorMessage = " Something went wrong.";
+            res.status(200).render("login", payload);
+            
+        });
+
+        if(user == null) {
+            //no user found
+            
+            var data = req.body;
+
+            data.password = await bcrypt.hash(password, 10)
+
+            User.create(data)
+            .then((user) => {
+                req.session.user = user;  
+                return res.redirect("/login");
+
+            })
+            .catch((error) => {
+                console.log(error);
+                payload.errorMessage = " Something went wrong.";
+                res.status(200).render("login", payload);
+                
+            });
+            
+        }
+        else {
+            //user found
+            if(email == user.email) {
+                payload.errorMessage = "Email already in use.";
+            }
+            else {
+                payload.errorMessage = "Username already in use.";
+            }
+            res.status(200).render("login", payload);
+
+        }
+
+    }
+    else {
+        payload.errorMessage = "Make sure each field has a valid value.";
+        res.status(200).render("login", payload);
+    }
+
     payload.errorMessage = "Make sure each field has a valid value.";
     res.status(200).render("login");
 })
+
+
 
 module.exports = router;
